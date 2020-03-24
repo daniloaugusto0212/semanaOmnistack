028b613ab220af52ccdeae9c7776a1f6a51f6b8a
Danilo Augusto <daniloaugusto0212@hotmail.com>
2020-03-24 15:23:44 -0300

    back

--- aulas/backend/index.js
+++ aulas/backend/index.js
@@ -1,12 +1,39 @@
-const express = required('express');
+const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) =>{
+app.use(express.json());
+
+/**
+ * Rota /Recurso
+ */
+
+ /**
+  * Métodos HTTP:
+  * 
+  * GET: Buscar/listar informação do back-end
+  * POST: Criar uma informação no back-end
+  * PUT: Alterar informações no bach-end
+  * DELETE: Deletar uma informação no back-end
+  */
+
+  /**
+   * Tipos de parâmetros
+   * 
+   * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
+   * Route Params: Parâmetros utilizados para identificar recursos
+   * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+   */
+
+app.post('/users', (request, response) =>{
+    const body = request.body;
+    
+    console.log(body);
+
     return response.json({
         evento:'Semana Omnistack 11.0',
         aluno: 'Danilo Augusto'
     }); 
 });
 
-app.listen(3333);
+app.listen(3332);
